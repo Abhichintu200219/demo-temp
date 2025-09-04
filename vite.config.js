3f5086da8473da17b8f05c0810cde959bba00cf4
abhiram.mb <abhiram.mb@ninestars.in>
2025-09-04 15:26:43 +0530

    updated pkggfd

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+export default defineConfig({
+    plugins: [react(), tailwindcss()],
+    test: {
+        exclude: [
+            '**/node_modules/**',
+            '**/e2e/**', // Exclude e2e directory
+            '**/*.spec.ts', // Exclude .spec.ts files
+        ]
+    },
+});
---
+++ vitest.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vitest/config';
+export default defineConfig({
+    test: {
+        globals: true, // enable describe/it/expect/vi as globals
+        environment: 'jsdom', // DOM for @testing-library
+        setupFiles: './src/setupTests.ts',
+        include: ['src/**/*.test.*', 'src/**/*.spec.*']
+    }
+});
